d6e84eba0a1cd7debf0e524fbd93a9a14a765970
zakuby <zakuby02@gmail.com>
2017-07-11 16:51:43 +0700

    Login Page Using Constraint Anchors

---
+++ GameOfChats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfChats
+//
+//  Created by admin on 7/11/17.
+//  Copyright © 2017 indosystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    func handleLogout(){
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+        
+    }
+
+}
+
