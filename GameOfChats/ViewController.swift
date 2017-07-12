9fdc90c23773d6195ce03594539254568fe41711
aadiprasetyo <adi.prasetyo96@gmail.com>
2017-07-12 11:58:01 +0700

    Firebase Register

--- GameOfChats/ViewController.swift
+++ GameOfChats/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+                
+        
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
